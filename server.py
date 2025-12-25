2c53df19d2ba090732dce740ce56d07180482bf2
Camila <cafer.blamey@gmail.com>
2025-12-25 19:10:47 -0300

    Correccion de error de sintaxis y robot 9x9

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-git add server.py
-git commit -m "Volviendo a la lista masiva de 25 empleos"
+git add .
+git commit -m "Correccion de error de sintaxis y robot 9x9"
 git push origin main
-3.  **Cuestiónate:** ¿Por qué ahora no necesitamos tocar `scraper.py`? Porque esta versión de `server.py` no usa al robot, tiene los datos guardados en su propia memoria. Es más rápido, pero no es "automático".
+3.  **Espera a Render:** Debería desplegarse correctamente esta vez.
